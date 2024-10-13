fb7caf7bb9ceef3628886c779aed3c6921c528df
tai44ba <shiita.taishi.s@gmail.com>
2024-10-13 19:24:24 +0200

    try to fix problem with pic and video

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,9 +1,25 @@
+// skills icon
+import htmlicon from '../imageAndVideo/icons8-html.svg'
+import cssicon from '../imageAndVideo/icons8-css3.svg'
+import JSicon from '../imageAndVideo/icons8-javascript.svg'
+import Reacticon from '../imageAndVideo/icons8-react-native.svg'
+import Nodeicon from '../imageAndVideo/icons8-nodejs.svg'
+import mongoicon from '../imageAndVideo/icons8-mongodb.svg'
+import Expressicon from '../imageAndVideo/icons8-expressjs.svg'
+import booticon from '../imageAndVideo/icons8-bootstrap.svg'
+import tailicon from '../imageAndVideo/icons8-tailwinds.svg'
+// project pictuure
+import MakeLottoImg from '../imageAndVideo/makeLottoNumberApp.png'
+import ToDoAppImg from '../imageAndVideo/icons8-tailwinds.svg'
+
+
+
 export const projects = [
   {
     id: 1,
     title: "Make Lotto Number App",
     description: "One can generate random number for lotto 6/49 and Euro Jeckpot",
-    image: "../src/assets/imageAndVideo/makeLottoNumberApp.png",
+    image: {MakeLottoImg} ,
     path:"https://makelottonumber.onrender.com",
     technology:'CSS Javascript React'
   },
@@ -11,23 +27,20 @@ export const projects = [
     id: 2,
     title: "To-Do App With Weather API",
     description: "One can create to-do list and also edit it, delete it and to see the weather where you chose",
-    image: "../src/assets/imageAndVideo/ToDoAppWithWeatherAPI.png",
+    image: {ToDoAppImg},
     path:"https://to-do-with-weather-api.onrender.com/",
     technology:'Javascript React Tailwind'
   },
 ];
 
 export const skills = [
-  { id: 1, name: "HTML", icon: "../src/assets/imageAndVideo/icons8-html.svg", path: 'https://www.w3schools.com/html/' },
-  { id: 2, name: "CSS", icon: "../src/assets/imageAndVideo/icons8-css3.svg", path: 'https://www.w3schools.com/css/default.asp' },
-  { id: 3, name: "JavaScript", icon: "../src/assets/imageAndVideo/icons8-javascript.svg", path: 'https://www.w3schools.com/js/default.asp' },
-  { id: 4, name: "React.js", icon: "../src/assets/imageAndVideo/icons8-react-native.svg", path: 'https://react.dev/' },
-  { id: 5, name: "Node.js", icon: "../src/assets/imageAndVideo/icons8-nodejs.svg", path: 'https://nodejs.org' },
-  { id: 6, name: "MongoDB", icon: "../src/assets/imageAndVideo/icons8-mongodb.svg", path: 'https://www.mongodb.com' },
-  { id: 7, name: "Express.js", icon: "../src/assets/imageAndVideo/icons8-expressjs.svg", path: 'https://expressjs.com/' },
-  { id: 8, name: "Bootstrap", icon: "../src/assets/imageAndVideo/icons8-bootstrap.svg", path: 'https://getbootstrap.com/' },
-  { id: 9, name: "Tailwind", icon: "../src/assets/imageAndVideo/icons8-tailwinds.svg", path: 'https://tailwindcss.com/' },
+  { id: 1, name: "HTML", icon:{htmlicon}, path: 'https://www.w3schools.com/html/' },
+  { id: 2, name: "CSS", icon: {cssicon}, path: 'https://www.w3schools.com/css/default.asp' },
+  { id: 3, name: "JavaScript", icon: {JSicon}, path: 'https://www.w3schools.com/js/default.asp' },
+  { id: 4, name: "React.js", icon: {Reacticon}, path: 'https://react.dev/' },
+  { id: 5, name: "Node.js", icon: {Nodeicon}, path: 'https://nodejs.org' },
+  { id: 6, name: "MongoDB", icon: {mongoicon}, path: 'https://www.mongodb.com' },
+  { id: 7, name: "Express.js", icon: {Expressicon}, path: 'https://expressjs.com/' },
+  { id: 8, name: "Bootstrap", icon: {booticon}, path: 'https://getbootstrap.com/' },
+  { id: 9, name: "Tailwind", icon: {tailicon}, path: 'https://tailwindcss.com/' },
 ];
-
-
-export const backgoundVideo = './imageAndVideo/27239-362518579_tiny.mp4'
